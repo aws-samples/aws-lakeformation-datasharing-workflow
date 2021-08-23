7b93547d53bedd7c8b9a11be84aad9d89f3b31a2
Jan Michael Go Tan <jantan@amazon.com>
2021-08-23 14:13:30 +0800

    Added first user creation

--- post-ui-deployment.js
+++ post-ui-deployment.js
@@ -1,16 +1,39 @@
 #!/usr/bin/env node
 
-const { AmplifyClient, UpdateAppCommand, CustomRule, GetAppCommand } = require("@aws-sdk/client-amplify");
-const { CognitoIdentityProviderClient, AdminCreateUserCommand } = require("@aws-sdk/client-cognito-identity-provider");
+const { AmplifyClient, UpdateAppCommand, GetAppCommand } = require("@aws-sdk/client-amplify");
+const { CognitoIdentityProviderClient, AdminCreateUserCommand, ListUserPoolsCommand } = require("@aws-sdk/client-cognito-identity-provider");
 const { IAMClient, PutRolePolicyCommand } = require("@aws-sdk/client-iam");
 
 const {fromIni} = require("@aws-sdk/credential-provider-ini");
 const localEnvInfo = require(__dirname+"/amplify/.config/local-env-info.json");
 const localAWSInfo = require(__dirname+"/amplify/.config/local-aws-info.json");
 const teamProviderInfo = require(__dirname+"/amplify/team-provider-info.json");
-const awsExports = require(__dirname+"/src/aws-exports.js");
 const inquirer = require("inquirer");
 const envName = localEnvInfo.envName;
+
+const findUserPoolIdForEnvironment = async(cognitoClient) => {
+    const auth = teamProviderInfo[envName].categories.auth;
+    const projId = Object.keys(auth)[0];
+
+    const parameters = require(__dirname+"/amplify/backend/auth/"+projId+"/parameters.json");
+    const userPoolName = parameters.userPoolName+"-"+envName;
+    let userPoolId = null;
+    let nextToken = null;
+
+    do {
+        const pools = await cognitoClient.send(new ListUserPoolsCommand({MaxResults: 60, NextToken: nextToken}));
+        nextToken = pools.NextToken;
+        pools.UserPools.forEach(p => {
+            if (p.Name === userPoolName) {
+                nextToken = null;
+                userPoolId = p.Id;
+            }
+        });
+    } while (nextToken != null);
+
+    return userPoolId;
+}
+
 const execPostUIDeployment = async() => {
     const awsInfo = localAWSInfo[envName];
     const clientParams = {};
@@ -47,14 +70,15 @@ const execPostUIDeployment = async() => {
     const amplifyClient = new AmplifyClient(clientParams);
     await amplifyClient.send(new UpdateAppCommand({appId: hosting.appId, customRules: [{source: "</^[^.]+$|\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|ttf|map|json)$)([^.]+$)/>", target:"/index.html", status: 200}]}));
     const appDetails = await amplifyClient.send(new GetAppCommand({appId: hosting.appId}));
-
+    
     console.log("Create First User");
     const username = await inquirer.prompt({message: "Enter Username", name: "value"});
     const email = await inquirer.prompt({message: "Enter Email", name: "value"});
 
     const cognitoClient = new CognitoIdentityProviderClient(clientParams);
+    const userPoolId = await findUserPoolIdForEnvironment(cognitoClient);
     await cognitoClient.send(new AdminCreateUserCommand({
-        UserPoolId: awsExports.aws_user_pools_id,
+        UserPoolId: userPoolId,
         Username: username.value,
         UserAttributes: [
             {
